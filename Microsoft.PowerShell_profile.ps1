6d50a6dac51d389788f2682bb88c380a57be0841
uttervitriol <uttervitriol@gmail.com>
2024-08-23 13:42:04 -0400

    nvim

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,10 +11,12 @@ Set-PSReadLineKeyHandler -Key Ctrl+RightArrow -ScriptBlock {
   $line = $null
   $cursor = $null
   [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-  if ($cursor -lt $line.Length) {
+  if ($cursor -lt $line.Length)
+  {
     [Microsoft.PowerShell.PSConsoleReadLine]::ForwardWord($key, $arg)
+  } else
+  { [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg) 
   }
-  else { [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg) }
 }
 
 ##################
@@ -30,20 +32,22 @@ Set-PSReadLineKeyHandler -Key Ctrl+u -Function BackwardDeleteLine
 ##################
 # ls -l
 ##################
-Function NoHidden { 
+Function NoHidden
+{ 
   param(
     [string[]]$path
   )
-  Get-ChildItem $path | where {$_.Name -NotLike ".*"}
+  Get-ChildItem $path | Where-Object {$_.Name -NotLike ".*"}
 }
 set-alias -name l -value NoHidden
 
 ##################
 # Alias that activates the vs developer shell
 ##################
-function MyDevx64 {
- &{ Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
-  Enter-VsDevShell 7eee0d47 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64";}
+function MyDevx64
+{
+  &{ Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
+    Enter-VsDevShell 7eee0d47 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64";}
 }
 Set-Alias -name devsh -value MyDevx64
 
@@ -51,15 +55,16 @@ Set-Alias -name devsh -value MyDevx64
 # Sets function and chord that changes to the directory
 # that lf was last on when quit
 ##################
-function lfcd{
+function lfcd
+{
   # lf keybind zh shows hidden files.
   lf -print-last-dir $args | Set-Location
 }
 
 Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
-         [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-         [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd')
-         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+  [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+  [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd')
+  [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
 ##################
@@ -78,6 +83,9 @@ Set-Alias -Name trash -Value Remove-ItemSafely
 Set-Alias -Name windbg -Value "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe"
 Set-Alias -Name windbg86 -Value "C:\Program Files (x86)\Windows Kits\10\Debuggers\x86\windbg.exe"
 
+#nvim
+Set-Alias -Name vim -Value nvim
+
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
@@ -85,7 +93,8 @@ Set-Alias -Name windbg86 -Value "C:\Program Files (x86)\Windows Kits\10\Debugger
 # for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path($ChocolateyProfile))
+{
   Import-Module "$ChocolateyProfile"
 }
 
